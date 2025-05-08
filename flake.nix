030d3197a3b4478b91fd804acdb2a7f9b7e99f72
aki-repo <andreast.work@gmail.com>
2025-05-08 18:52:55 -0400

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Customized dwm";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      overlay = final: prev: {
+        dwm = prev.dwm.overrideAttrs (old: {
+          version = "6.5";
+          src = builtins.path { path = ./.; name = "dwm"; };
+        });
+      };
+
+      dwm = (
+        import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        }
+      ).dwm;
+    in
+    {
+      overlays.default = overlay;
+
+      packages.${system}.default = dwm;
+
+      checks.${system} = {
+        build = dwm;
+
+        version = nixpkgs.legacyPackages.${system}.runCommand "version-check" { } ''
+          dwm_version="$(${dwm}/bin/dwm -v 2>&1 || :)"
+
+          echo "package version: ${dwm.name}"
+          echo "dwm version:     $dwm_version"
+
+          [[ "${dwm.name}" == "$dwm_version" ]]
+          touch ${placeholder "out"}
+        '';
+      };
+    };
+}
